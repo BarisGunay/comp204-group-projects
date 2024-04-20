7a10ff6536ac82778ad18e72b624e0d8008f5d9c
MeteMentese <mete.mentese@gmail.com>
2024-04-21 01:32:00 +0300

    score
    right side added with score

--- Tetris_2048_Project/Tetris_2048.py
+++ Tetris_2048_Project/Tetris_2048.py
@@ -16,18 +16,21 @@ import random  # used for creating tetrominoes with random types (shapes)
 def start():
    # set the dimensions of the game grid
    grid_h, grid_w = 20, 12
+
+   right_panel_width = 5
+
    # set the size of the drawing canvas (the displayed window)
-   canvas_h, canvas_w = 40 * grid_h, 40 * grid_w
+   canvas_h, canvas_w = 40 * grid_h, 40 * (grid_w + right_panel_width)
    stddraw.setCanvasSize(canvas_w, canvas_h)
    # set the scale of the coordinate system for the drawing canvas
-   stddraw.setXscale(-0.5, grid_w - 0.5)
+   stddraw.setXscale(-0.5, grid_w + right_panel_width - 0.5)
    stddraw.setYscale(-0.5, grid_h - 0.5)
 
    # set the game grid dimension values stored and used in the Tetromino class
    Tetromino.grid_height = grid_h
    Tetromino.grid_width = grid_w
    # create the game grid
-   grid = GameGrid(grid_h, grid_w)
+   grid = GameGrid(grid_h, grid_w, right_panel_width)
    # create the first tetromino to enter the game grid
    # by using the create_tetromino function defined below
    current_tetromino = create_tetromino()
@@ -35,7 +38,7 @@ def start():
 
    # display a simple menu before opening the game
    # by using the display_game_menu function defined below
-   display_game_menu(grid_h, grid_w)
+   display_game_menu(grid_h, grid_w + right_panel_width)
 
    # the main game loop
    while True:
